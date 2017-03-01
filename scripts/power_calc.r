03030d166af8fc3c57c6923805f9e41e4da39b43
Gibran Hemani <explodecomputer@gmail.com>
2017-03-01 11:21:25 +0000

    updated

--- scripts/power_calc.r
+++ scripts/power_calc.r
@@ -1,115 +1,4 @@
-# library(pROC)
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-
-make_geno <- function(nid, nsnp, maf)
-{
-	return(matrix(rbinom(nid * nsnp, 2, maf), nid, nsnp))
-}
-
-range01 <- function(x)
-{
-	(x-min(x))/(max(x)-min(x))
-}
-
-gx_to_gp <- function(gx, h2x, prev)
-{
-	x_prime <- qnorm(prev, 0, 1)
-	p <- pnorm(x_prime, mean=gx, sd = sqrt(1 - h2x), lower.tail=FALSE)
-	return(p)
-}
-
-
-# G should scaled
-# var(eff) is the h2x
-simulate <- function(G, eff, prevalence, prop_discovered)
-{
-	nid <- nrow(G)
-	nsnp <- ncol(G)
-	h2x <- var(eff)
-	gx_true <- as.numeric(scale(G %*% eff)) * sqrt(h2x)
-	prob_disease <- gx_to_gp(gx_true, h2x, 1-prevalence)
-	disease <- rbinom(nid, 1, prob_disease)
-	eff_pred <- eff
-	eff_pred[sample(1:nsnp, nsnp * (1-prop_discovered))] <- 0
-	gx_pred <- as.numeric(G %*% eff_pred / sqrt(nsnp))
-	dat <- data.frame(gx_true=gx_true, gx_pred=gx_pred, prob_disease=prob_disease, disease=disease)
-	return(dat)
-}
-
-make_system <- function(nid, prev, h2disease, h2pred, vpred)
-{
-	prs_disease <- rnorm(nid)
-	prs_pred <- rnorm(nid)
-	pred <- makePhen(sqrt(h2pred), prs_pred)
-	e <- rnorm(nid)
-	ve <- 1 - h2disease - vpred
-
-	px <- scale(prs_disease * sqrt(h2disease) + pred * sqrt(vpred) + e * sqrt(ve))
-	prob_disease <- gx_to_gp(px, 1, 1-prev)
-	disease <- rbinom(nid, 1, prob_disease)
-	dat <- data.frame(
-		disease=disease,
-		prob_disease=prob_disease,
-		prs_disease=prs_disease,
-		pred=pred,
-		prs_pred=prs_pred
-	)
-}
-
-
-do_test <- function(dat)
-{
-	dat <- filter(dat, disease==1)
-	a <- fastAssoc(dat$prs_disease, dat$pred)
-	b <- fastAssoc(dat$prs_disease, dat$prs_pred)
-	ab <- rbind(as.data.frame(a), as.data.frame(b))
-	ab$model <- c("RF", "PRS of RF")
-	return(ab)
-}
-
-
-makePhen <- function(effs, indep, vy=1, vx=rep(1, length(effs)), my=0)
-{
-	if(is.null(dim(indep))) indep <- cbind(indep)
-	stopifnot(ncol(indep) == length(effs))
-	stopifnot(length(vx) == length(effs))
-	cors <- effs * vx / sqrt(vx) / sqrt(vy)
-	stopifnot(sum(cors^2) <= 1)
-	cors <- c(cors, sqrt(1-sum(cors^2)))
-	indep <- t(t(scale(cbind(indep, rnorm(nrow(indep))))) * cors * c(vx, 1))
-	y <- drop(scale(rowSums(indep)) * sqrt(vy)) + my
-	return(y)
-}
-
-
-fastAssoc <- function(y, x)
-{
-	index <- is.finite(y) & is.finite(x)
-	n <- sum(index)
-	y <- y[index]
-	x <- x[index]
-	vx <- var(x)
-	bhat <- cov(y, x) / vx
-	ahat <- mean(y) - bhat * mean(x)
-	# fitted <- ahat + x * bhat
-	# residuals <- y - fitted
-	# SSR <- sum((residuals - mean(residuals))^2)
-	# SSF <- sum((fitted - mean(fitted))^2)
-
-	rsq <- (bhat * vx)^2 / (vx * var(y))
-	fval <- rsq * (n-2) / (1-rsq)
-	tval <- sqrt(fval)
-	se <- abs(bhat / tval)
-
-	# Fval <- (SSF) / (SSR/(n-2))
-	# pval <- pf(Fval, 1, n-2, lowe=F)
-	p <- pf(fval, 1, n-2, lowe=F)
-	return(list(
-		ahat=ahat, bhat=bhat, se=se, fval=fval, pval=p, n=n
-	))
-}
+source("functions.r")
 
 #####
 
@@ -123,7 +12,23 @@ param <- expand.grid(
 )
 param$nid <- param$ncase / param$prev
 
-set.seed(100)
+
+
+arguments <- commandArgs(T)
+jid <- as.numeric(arguments[1])
+chunks <- as.numeric(arguments[2])
+out <- arguments[3]
+
+chunksize <- ceiling(nrow(param) / chunks)
+t1 <- (jid - 1) * chunksize + 1
+t2 <- min(jid * chunksize, nrow(param))
+
+message("total size: ", nrow(param))
+message("running: ", t1, " to ", t2)
+
+param <- param[t1:t2, ]
+
+
 res <- list()
 for(i in 1:nrow(param))
 {
@@ -136,5 +41,5 @@ for(i in 1:nrow(param))
 
 res <- bind_rows(res)
 
-save(res, file="../results/power_calc.rdata")
+save(res, file=out)
 
---
+++ scripts/power_calc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PBS -N case_collider
+#PBS -o job_reports/casecollider-output
+#PBS -e job_reports/casecollider-error
+#PBS -l walltime=12:00:00
+#PBS -t 1-100
+#PBS -l nodes=1:ppn=1
+#PBS -S /bin/bash
+
+set -e
+
+echo "Running on ${HOSTNAME}"
+
+if [ -n "${1}" ]; then
+	echo "${1}"
+	PBS_ARRAYID=${1}
+fi
+
+i=${PBS_ARRAYID}
+splits=16
+
+Rscript \
+	power_calc.r \
+	${i} \
+	${splits} \
+	../results/power_calc_${i}.rdata
